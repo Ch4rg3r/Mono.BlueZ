0177331e9e7ad8ade0c6de111661cdb05633639b
Ch4rg3r <ch4rg3r@hotmail.de>
2016-04-19 16:45:12 +0200

    Media
    Media Interfaces for sink

---
+++ Mono.BlueZ.DBus/MediaEndpoint1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using DBus;
+
+namespace Mono.BlueZ.DBus
+{
+    // on /org/bluez/hciX
+    [Interface("org.bluez.MediaEndpoint1")]
+    public interface MediaEndpoint1
+    {
+        void SetConfiguration(ObjectPath transport, IDictionary<string,object> properties);
+		byte[] SelectConfiguration(byte[] capabilities);
+		void ClearConfiguration(ObjectPath transport);
+        void Release();
+    }
+}
---
+++ Mono.BlueZ.DBus/MediaTransport1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using DBus;
+
+namespace Mono.BlueZ.DBus
+{
+    // on /org/bluez/hciX
+    [Interface("org.bluez.MediaEndpoint1")]
+    public interface MediaEndpoint1
+    {
+        FileDescriptor Acquire();
+		FileDescriptor TryAcquire();
+        void Release();
+        readonly ObjectPath Device;
+        readonly string UUID;
+		readonly byte Codec ;
+        readonly byte[] Configuration;
+        readonly State State;
+        ushort Delay {get;set;}
+        ushort Volume {get;set;}
+    }
+
+    public enum State
+    {
+        Idle = "idle",
+        Pending = "pending",
+        Active = "active"
+    }
+}
